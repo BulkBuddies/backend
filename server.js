9461790a1c25db244a17e915b3a57a2cb6514f3f
Felipe Castillo <felipecastillo48@gmail.com>
2024-04-09 20:14:52 -0400

    Initial commit:Set up backend strcuture

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ Message: "Welcome" });
+});
+
+app.listen(PORT, () => {
+  console.log(`LISTENING ON ${PORT}`);
+});
