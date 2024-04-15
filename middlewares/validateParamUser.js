1310e9ec1320c92ec5293c4bb083ef05404440d1
Gabriel Larez <gabrielnoriega@gmail.com>
2024-04-15 19:20:09 -0400

    actualizar validar

--- middlewares/validateParamUser.js
+++ middlewares/validateParamUser.js
@@ -1,10 +1,7 @@
 const validateParamUser = (req, res, next) => {
-    const { email, name, last_name, phone, name_user, password, password_repeat } = req.body;
-    if (!email || !name || !last_name || !phone ||!name_user ||!password ||!password_repeat) {
+    const { first_name, last_name, email, username, password } = req.body;
+    if (!first_name || !last_name || !email || !username ||!password) {
         return res.status(400).json({ error: "Debe llenar todos los campos" });
-    }
-    if(password !== password_repeat){
-        return res.status(400).json({ error: "claves deben ser iguales" });
     }
     next();
 }
