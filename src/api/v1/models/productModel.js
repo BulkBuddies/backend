b0ef03b7a90865eb11af5da07001d2aa799f144e
fpobleteinfo <fpobleteinfo@gmail.com>
2024-04-18 20:43:59 -0400

    manejo del error con next en product

--- src/api/v1/models/productModel.js
+++ src/api/v1/models/productModel.js
@@ -2,8 +2,7 @@ import pool from "../../../../config/db/db.js";
 import format from "pg-format";
 import { createNewError } from "../helpers/requestError.js";
 
-
-export const getAllPro = async () => {
+const getAllPro = async () => {
   try {
     const sqlQuery = {
       text: "SELECT id, name, description, required_stock, unit_price, created_at, updated_at, url, category_id FROM product",
@@ -11,21 +10,24 @@ export const getAllPro = async () => {
     const users = await pool.query(sqlQuery);
     return users.rows;
   } catch (error) {
-    console.log(error);
+    throw createNewError(error.code);
   }
 };
 
-
-export const getProductById = async (id) => {
-  const sqlQuery = {
-    text: "SELECT id, name, description, required_stock, unit_price, created_at, updated_at, url, category_id FROM product where id = $1",
-    values: [id],
-  };
-  const { rows } = await pool.query(sqlQuery);
-  return rows[0];
+const getProductById = async (id) => {
+  try {
+    const sqlQuery = {
+      text: "SELECT id, name, description, required_stock, unit_price, created_at, updated_at, url, category_id FROM product where id = $1",
+      values: [id],
+    };
+    const { rows } = await pool.query(sqlQuery);
+    return rows[0];
+  } catch (error) {
+    throw createNewError(error.code);
+  }
 };
 
-export const getCategoryName = async (id) => {
+const getCategoryName = async (id) => {
   const sqlQuery = {
     text: "SELECT name FROM category where id = $1",
     values: [id],
@@ -34,8 +36,7 @@ export const getCategoryName = async (id) => {
   return rows[0];
 };
 
-
-export const getProductByCategoryId = async (category_id) => {
+const getProductByCategoryId = async (category_id) => {
   const sqlQuery = {
     text: "SELECT id, name, description, required_stock, unit_price, created_at, updated_at, url, category_id FROM product where category_id = $1",
     values: [category_id],
@@ -44,20 +45,26 @@ export const getProductByCategoryId = async (category_id) => {
   return rows;
 };
 
-export const getProLimitOrder = async (
-    field = "name",
-    direction = "ASC",
-    limit = 10,
-    page = 0
-  ) => {
-    const offset = Math.abs((page - 1) * limit)
-    const formattedQuery = format(
-      "SELECT * FROM product ORDER BY %s %s LIMIT %s OFFSET %s",
-      field,
-      direction,
-      limit,
-      offset
-    );
-    const result = await pool.query(formattedQuery);
-    return result.rows;
-  };
+const getProLimitOrder = async (
+  field = "name",
+  direction = "ASC",
+  limit = 10,
+  page = 0
+) => {
+  const offset = Math.abs((page - 1) * limit);
+  const formattedQuery = format(
+    "SELECT * FROM product ORDER BY %s %s LIMIT %s OFFSET %s",
+    field,
+    direction,
+    limit,
+    offset
+  );
+  const result = await pool.query(formattedQuery);
+  return result.rows;
+};
+
+// export getProductFilter = async ({}) =>{
+
+// }
+
+export { getAllPro,getProductById,getCategoryName, getProLimitOrder, getProductByCategoryId  }
