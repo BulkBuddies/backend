434f79b3dc0960aad6e54f849261d6f07997744d
Felipe Castillo <felipecastillo48@gmail.com>
2024-04-27 09:37:41 -0400

    BREAKING CHANGE: product route will no longer be used

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -28,6 +28,7 @@ const verifyJWT = async (req, res, next) => {
 const validateToken = async (token, secretKey) => {
   try {
     const decoded = jwt.verify(token, secretKey);
+
     return decoded;
   } catch (err) {
     throw createNewError(err.message);
