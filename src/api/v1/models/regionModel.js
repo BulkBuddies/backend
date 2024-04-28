ad0554743d41b59c51fb5685f8d338fc56b841a5
Felipe Castillo <felipecastillo48@gmail.com>
2024-04-27 22:24:11 -0400

    chore:remove try catch from region models to let controller handle errors

--- src/api/v1/models/regionModel.js
+++ src/api/v1/models/regionModel.js
@@ -2,28 +2,20 @@ import pool from "../../../../config/db/db.js";
 import { createNewError } from "../helpers/requestError.js";
 
 const getAllRegions = async () => {
-  try {
-    const sqlQuery = {
-      text: "SELECT id, region FROM regiones",
-    };
-    const regions = await pool.query(sqlQuery);
-    return regions.rows;
-  } catch (error) {
-    throw createNewError(error.code);
-  }
+  const sqlQuery = {
+    text: "SELECT id, region FROM regiones",
+  };
+  const regions = await pool.query(sqlQuery);
+  return regions.rows;
 };
 
 const getComunaByRegion = async (region_id) => {
-  try {
-    const sqlQuery = {
-      text: "SELECT id, comuna, region_id FROM comunas WHERE region_id = $1",
-      values: [region_id],
-    };
-    const { rows } = await pool.query(sqlQuery);
-    return rows;
-  } catch (error) {
-    throw createNewError(error.code);
-  }
+  const sqlQuery = {
+    text: "SELECT id, comuna, region_id FROM comunas WHERE region_id = $1",
+    values: [region_id],
+  };
+  const { rows } = await pool.query(sqlQuery);
+  return rows;
 };
 
 export { getAllRegions, getComunaByRegion };
