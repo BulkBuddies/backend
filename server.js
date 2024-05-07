4f2e279dab17762ac737949f87382502161475f6
Felipe Castillo <felipecastillo48@gmail.com>
2024-05-07 18:49:44 -0400

    temp: cors with options

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ import { MemoryStore } from "express-session";
 import chalk from "chalk";
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 PRODUCTION_ENV && app.use(logger());
 
